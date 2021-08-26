e89cca12b75ab9a26736685c32b297d8e3e905fd
Susanna Häggblom <susanna.haggblom@genero.fi>
2021-08-26 15:10:37 +0300

    rename plugin

--- gravityforms-traffic-source-tracking.php
+++ gravityforms-traffic-source-tracking.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name:        Initial traffic source tracking with Gravity Forms
+Plugin Name:        Gravity Forms: Initial traffic source tracking
 Plugin URI:         http://genero.fi
 Description:        A WordPress plugin
-Version:            0.0.1
+Version:            1.0.0
 Author:             Genero
 Author URI:         http://genero.fi/
 License:            MIT License
